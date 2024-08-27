66ee1b6d9d87c7d06fae8a07148fc8e9b40a8d3c
maxDavid40 <maxdavid40@gmail.com>
2024-08-27 15:12:14 +0200

    FIX run_rpc_server overload

--- src/happy_vllm/rpc/server.py
+++ src/happy_vllm/rpc/server.py
@@ -9,8 +9,8 @@ from vllm.entrypoints.openai.rpc.server import AsyncEngineRPCServer, run_server
 
 
 def run_rpc_server(async_engine_args: AsyncEngineArgs,
-                   usage_context: UsageContext, port: int):
-    server = AsyncEngineRPCServer(async_engine_args=async_engine_args, usage_context=usage_context, port=port)
+                   usage_context: UsageContext, rpc_path: str):
+    server = AsyncEngineRPCServer(async_engine_args=async_engine_args, usage_context=usage_context, rpc_path=rpc_path)
     model_consumed_memory = Gauge("model_memory_usage", "Model Consumed GPU Memory in GB ")
     model_consumed_memory.set(round(server.engine.engine.model_executor.driver_worker.model_runner.model_memory_usage/float(2**30),2)) # type: ignore
     asyncio.run(run_server(server))
