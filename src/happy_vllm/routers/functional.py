c9ad83fe47c4a6bd6168f24aca41c56db16b7522
maxDavid40 <maxdavid40@gmail.com>
2025-02-24 11:58:13 +0100

    update code to remove generate and generate's utils

--- src/happy_vllm/routers/functional.py
+++ src/happy_vllm/routers/functional.py
@@ -36,13 +36,10 @@ from vllm.entrypoints.openai.serving_embedding import OpenAIServingEmbedding
 from vllm.entrypoints.openai.serving_tokenization import OpenAIServingTokenization
 
 from happy_vllm import utils
-from happy_vllm.logits_processors.response_pool import VLLMLogitsProcessorResponsePool
-from happy_vllm.logits_processors.utils_parse_logits_processors import logits_processors_parser, detect_logits_processors_incompatibilities
 
 from ..model.model_base import Model
 from ..core.resources import RESOURCE_MODEL, RESOURCES
 from happy_vllm.routers.schemas import functional as functional_schema
-
 
 
 # Load the response examples
@@ -53,79 +50,6 @@ with open(request_examples_path, 'r') as file:
 
 # Functional router
 router = APIRouter()
-
-
-def parse_logits_processors(request_dict: dict, prompt: str, model: AsyncLLMEngine, tokenizer: PreTrainedTokenizerBase,
-                            tokenizer_lmformatenforcer: TokenEnforcerTokenizerData) -> list:
-    """Parses the body of the request in order to provide the logits processors
-    
-    Args:
-        request_dict (dict): The body of the request
-        model (AsyncLLMEngine): The model
-        tokenizer : The tokenizer
-        tokenizer_lmformatenforcer : The LM format enforcer version of the tokenizer
-
-    Returns:
-        list : The list of logits processors
-    """
-    logits_processors = []
-    references = {'prompt': prompt, 'model': model, 'tokenizer': tokenizer, 'tokenizer_lmformatenforcer': tokenizer_lmformatenforcer}
-    list_keyword = list(request_dict)
-    for keyword_main in list_keyword:
-        # Parse the body sent via API as explained in happy_vllm.logits_processors.utils_parse_logits_processors
-        if keyword_main in logits_processors_parser:
-            dict_logits_processor = logits_processors_parser[keyword_main]
-            class_to_instantiate = dict_logits_processor['class']
-            kwargs = {}     
-            # Add prompt, model or tokenizer if needed
-            for reference, refered_object in references.items():
-                if dict_logits_processor.get(reference, False):
-                    kwargs[reference] = refered_object
-            # Add arguments to instantiate the logits_processor if needed
-            for keyword, class_argument in dict_logits_processor['arguments'].items():
-                if keyword in request_dict:
-                    kwargs[class_argument] = request_dict.pop(keyword)
-            logit_processor = class_to_instantiate(**kwargs)
-            logits_processors.append(logit_processor)
-    # To ensure that no unsuitable keywords are passed to vllm, we pop them
-    for keyword_main, logits_processor_config in logits_processors_parser.items():
-        for keyword in logits_processor_config['arguments']:
-            if keyword in request_dict:
-                request_dict.pop(keyword)
-    return logits_processors
-
-
-def parse_generate_parameters(request_dict: dict, model: AsyncLLMEngine, tokenizer: PreTrainedTokenizerBase,
-                            tokenizer_lmformatenforcer: TokenEnforcerTokenizerData) -> Tuple[str, bool, SamplingParams]:
-    """Parses the body of the request to obtain the prompt and the sampling parameters
-
-    Args:
-        request_dict (dict): The body of the request
-        model (AsyncLLMEngine): The model
-        tokenizer : The tokenizer
-        tokenizer_lmformatenforcer : The LM format enforcer version of the tokenizer
-    Returns:
-        str : The prompt
-        bool : Whether the prompt should be displayed in the response
-        SamplingParams : The vllm sampling parameters
-    """
-    prompt = request_dict.pop("prompt")
-    if 'prompt_in_response' in request_dict:
-        prompt_in_response = request_dict.pop('prompt_in_response')
-    else:
-        prompt_in_response = False
-    multi_modal_data = request_dict
-    detect_logits_processors_incompatibilities(request_dict)
-    logits_processors = parse_logits_processors(request_dict, prompt, model, tokenizer, tokenizer_lmformatenforcer)
-    sampling_params = SamplingParams(**request_dict)
-    sampling_params.logits_processors = logits_processors
-    for logits_processor in logits_processors:
-        if isinstance(logits_processor, VLLMLogitsProcessorResponsePool):
-            min_tokens = sampling_params.min_tokens
-            reponse_pool = logits_processor.possible_tokens_responses
-            if min_tokens > 0 and len(reponse_pool):
-                raise ValueError(f"min_tokens : {min_tokens} is incompatible with the `response_pool` keyword")
-    return prompt, prompt_in_response, sampling_params
 
 
 def verify_request(request: Union[
@@ -183,94 +107,6 @@ def check_generator(generator: Union[
 
 def base(request: Request, model: Model) -> OpenAIServing:
     return model.openai_serving_tokenization
-
-
-@router.post("/v1/generate", response_model=functional_schema.ResponseGenerate)
-async def generate(
-    request: Request,
-    request_type: Annotated[
-        functional_schema.RequestGenerate,
-        Body(openapi_examples=request_openapi_examples["generate"])] = None
-    ) -> Response:
-    """DEPRECATED. Generate completion for the request.
-
-    The request should be a JSON object with the following fields:
-    - prompt: The prompt to use for the generation.
-    - other fields: The sampling parameters (See `SamplingParams` from vLLM for more details : 
-                                             https://github.com/vllm-project/vllm/blob/main/vllm/sampling_params.py).
-    """
-    
-    model: Model = RESOURCES[RESOURCE_MODEL]
-    request_dict = await request.json()
-    prompt, prompt_in_response, sampling_params = parse_generate_parameters(request_dict, model._model, model._tokenizer, model._tokenizer_lmformatenforcer)
-    request_id = random_uuid()
-    model._tokenizer.truncation_side = model.original_truncation_side
-    results_generator = model._model.generate(prompt, sampling_params, request_id)
-
-    # Non-streaming case
-    final_output = None
-    async for request_output in results_generator:
-        if await request.is_disconnected():
-            # Abort the request if the client disconnects.
-            await model._model.abort(request_id)
-            return Response(status_code=499)
-        final_output = request_output
-
-    if final_output is None:
-        raise ValueError('The final ouput is None')
-    prompt = final_output.prompt
-    text_outputs = [output.text for output in final_output.outputs]
-    prompt_nb_tokens = len(final_output.prompt_token_ids)
-    nb_tokens_outputs = [len(output.token_ids) for output in final_output.outputs]
-    usages = [{"prompt_tokens": prompt_nb_tokens,
-               "completion_tokens": completion_tokens,
-               "total_tokens": prompt_nb_tokens + completion_tokens } for completion_tokens in nb_tokens_outputs]
-    finish_reasons = [output.finish_reason for output in request_output.outputs]
-    finish_reasons = ["None" if finish_reason is None else finish_reason for finish_reason in finish_reasons]
-    ret = {"responses": text_outputs, "finish_reasons": finish_reasons, "usages": usages}
-    if prompt_in_response:
-        ret['prompt'] = prompt
-    return JSONResponse(ret)
-
-
-@router.post("/v1/generate_stream", response_model=functional_schema.ResponseGenerate)
-async def generate_stream(request: Request,
-    request_type: Annotated[
-        functional_schema.RequestGenerate,
-        Body(openapi_examples=request_openapi_examples["generate_stream"])] = None
-    ) -> StreamingResponse:
-    """DEPRECATED. Generate completion for the request.
-
-    The request should be a JSON object with the following fields:
-    - prompt: The prompt to use for the generation.
-    - other fields: The sampling parameters (See `SamplingParams` for details).
-    """
-    model: Model = RESOURCES[RESOURCE_MODEL]
-    request_dict = await request.json()
-    prompt, prompt_in_response, sampling_params = parse_generate_parameters(request_dict, model._model, model._tokenizer, model._tokenizer_lmformatenforcer)
-    request_id = random_uuid()
-    model._tokenizer.truncation_side = model.original_truncation_side
-    results_generator = model._model.generate(prompt, sampling_params, request_id)
-
-    async def stream_results() -> AsyncGenerator[str, None]:
-        async for request_output in results_generator:
-            prompt = request_output.prompt
-            text_outputs = [
-                output.text for output in request_output.outputs
-            ]
-            finish_reasons = [output.finish_reason for output in request_output.outputs]
-            finish_reasons = ["None" if finish_reason is None else finish_reason for finish_reason in finish_reasons]
-            prompt_nb_tokens = len(request_output.prompt_token_ids)
-            nb_tokens_outputs = [len(output.token_ids) for output in request_output.outputs]
-            usages = [{"prompt_tokens": prompt_nb_tokens,
-                    "completion_tokens": completion_tokens,
-                    "total_tokens": prompt_nb_tokens + completion_tokens } for completion_tokens in nb_tokens_outputs]
-            ret = {"responses": text_outputs, "finish_reasons": finish_reasons, "usages": usages}
-            if prompt_in_response:
-                ret['prompt'] = prompt
-            yield (json.dumps(ret) + "\n")#.encode("utf-8")
-
-    return StreamingResponse(stream_results())
 
 
 @router.post("/v1/tokenizer", response_model=functional_schema.ResponseTokenizer)
